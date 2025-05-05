fdf310c5244ea33bb2a28fa609ca8dfa6a51ba5e
chinmaynawkar <119050434+chinmaynawkar@users.noreply.github.com>
2025-05-05 12:29:51 +0530

    Merge pull request #2 from chinmaynawkar/feature/T2_implement_home_page
    feat:added ui for home page with Interview Card

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="root-layout">{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">AI Interview Platform</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
+  );
 };
 
 export default RootLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,63 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { dummyInterviews } from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-white">Home Page</h1>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real-time interview questions and get instant feedback
+            to improve your skills.
+          </p>
+          <Button asChild className="btn-primary-max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robot-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section flex flex-row flex-wrap gap-4">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              key={interview.id}
+              interviewId={interview.id}
+              role={interview.role}
+              type={interview.type}
+              techstack={interview.techstack}
+              createdAt={interview.createdAt}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take a Mock Interview</h2>
+        <div className="interviews-section flex flex-row flex-wrap gap-4">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              key={interview.id}
+              interviewId={interview.id}
+              role={interview.role}
+              type={interview.type}
+              techstack={interview.techstack}
+              createdAt={interview.createdAt}
+            />
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
